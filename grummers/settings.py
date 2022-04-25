9af61f48a7ced9f8675882e55108e14933227f16
illi-homz <gopkabel@mail.ru>
2022-04-25 10:55:21 +0300

    fix

--- grummers/settings.py
+++ grummers/settings.py
@@ -27,8 +27,8 @@ DEBUG = False
 # DEBUG = True
 
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://*.herokuapp.com']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://grummers-test.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
