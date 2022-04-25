c5d6d9d249e4681fc60ccbf6a0f9eb319dfc8d36
illi-homz <gopkabel@mail.ru>
2022-04-25 10:21:26 +0300

    fix

--- grummers/urls.py
+++ grummers/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('', include('app.urls'), name='main-view'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'app.views.handle_page_not_found'
