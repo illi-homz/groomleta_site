496ec3418b8dafb2e304f41004a47690464e7709
illi-homz <gopkabel@mail.ru>
2022-04-25 09:31:56 +0300

    fix

--- grummers/settings.py
+++ grummers/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = env('SECRET_KEY')
 # DEBUG = False
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
