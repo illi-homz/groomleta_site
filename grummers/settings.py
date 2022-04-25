23ab60f82e6adb654d1123d59898cc416353b4e1
illi-homz <gopkabel@mail.ru>
2022-04-25 10:09:29 +0300

    fix

--- grummers/settings.py
+++ grummers/settings.py
@@ -114,11 +114,11 @@ USE_TZ = False
 STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/mediafiles/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "staticfiles"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
