b198498882a109c166982c95395568bf3645a4c8
illi-homz <gopkabel@mail.ru>
2022-04-25 09:33:31 +0300

    fix

--- grummers/settings.py
+++ grummers/settings.py
@@ -22,8 +22,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'grummers-test.herokuapp.com']
 
