22d85fe6a7ef458f9c819b5b4c8e3214583e3010
illi <gopkabel@mail.ru>
2022-08-21 23:31:17 +0300

    fix

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -8238,8 +8238,8 @@ grummer.popupMain = {
       return true;
     });
     if (!imgs.length) return;
-    this.images = [...this.images, ...imgs];
-    console.log('this.images', this.images);
+    this.images = [...this.images, ...imgs]; // console.log('this.images', this.images);
+
     var dt = new DataTransfer();
     imgs.forEach(img => dt.items.add(img));
     el.files = dt.files;
