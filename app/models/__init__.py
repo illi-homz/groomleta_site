e57c78808c540a6c9f81779e76ad230e757c86b0
illi <gopkabel@mail.ru>
2022-08-27 15:09:09 +0300

    fix migrations

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,12 +7,12 @@ from .Callback import Callback
 from .ServiceRecord import ServiceRecord
 from .Service import Service
 from .Сategory import Сategory
-from .Master import Master
 from .Breed import Breed
+from .OurSalon import OurSalon
+from .Master import Master
 from .Event import Event
 from .Client import Client
 from .Product import Product
 from .OrderService import OrderService
 from .OrderProduct import OrderProduct
 from .Order import Order
-from .OurSalon import OurSalon
