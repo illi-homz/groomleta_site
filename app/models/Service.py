d97cfd94c5f8a3abfaf6036ad545b18070c556e0
illi-homz <gopkabel@mail.ru>
2024-01-25 17:58:28 +0300

    fix index data

--- app/models/Service.py
+++ app/models/Service.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils.html import mark_safe
 from .Category import Category
 from .Breed import Breed
-from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import FileExtensionValidator
 
